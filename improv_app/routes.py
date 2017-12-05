a2ecd52572fb7b6583d9caa4be339eb135a14c2c
yejack <yeshuchuan000@gmail.com>
2017-12-04 23:39:28 -0500

    213

--- improv_app/routes.py
+++ improv_app/routes.py
@@ -86,7 +86,7 @@ def browse():
 
 @app.route("/chatroom")
 def chatroom():
-	return render_template("/../index.html")
+	return render_template("/../../templates/index.html")
 
 @app.route("/chatroom_video")
 def chatroom_video():
